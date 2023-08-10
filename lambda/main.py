209b6cd1dc8b489b17972c8fc5a85d31f8ab0a56
vudiep411 <vudiep411@gmail.com>
2023-08-10 11:38:45 -0700

    fix lambda function handler

--- lambda/main.py
+++ lambda/main.py
@@ -4,10 +4,10 @@ def lambda_handler(event, context):
     print(event)
     message = 'Hello from Lambda!'
     response = {
-        "statuscode": 200,
-        "headers" : {
-            'Content-Type': 'application/json'
-        },
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+         },
         "body": json.dumps(message)
     }
     return response
