5b546d55093e03f9917b8ba61d996f11864bbedc
vudiep411 <vudiep411@gmail.com>
2023-08-10 11:08:12 -0700

    deployment api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -52,3 +52,10 @@ resource "aws_api_gateway_integration_response" "example_integration_response" {
   }
 }
 
+resource "aws_api_gateway_deployment" "example_deployment" {
+  depends_on = [aws_api_gateway_integration.example_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  stage_name  = "dev"
+
+}
